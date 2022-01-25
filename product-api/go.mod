66465a753a4c4028819092c6626c2ad8dbfadd0e
Christina Zhang <57076552+christinazhangstudio@users.noreply.github.com>
2022-01-25 15:34:33 -0600

    Add files via upload

---
+++ product-api/go.mod
@@ -0,0 +1,17 @@
+module github.tesla.com/chrzhang/go-microservices-restful/product-api
+
+go 1.15
+
+require (
+	github.com/go-openapi/errors v0.20.1
+	github.com/go-openapi/runtime v0.21.1
+	github.com/go-openapi/strfmt v0.21.0
+	github.com/go-openapi/swag v0.19.15
+	github.com/go-openapi/validate v0.20.3
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/stretchr/testify v1.7.0
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
---
+++ product-api/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"os"
+	"github.com/go-openapi/runtime/middleware"
+	"time"
+	"context"
+	"os/signal"
+	"github.com/gorilla/mux"
+
+	"github.tesla.com/chrzhang/go-microservices-restful/product-api/handlers"
+	"github.tesla.com/chrzhang/go-microservices-restful/product-api/data"
+)
+
+func main() {
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	v := data.NewValidation()
+
+	ph := handlers.NewProducts(l, v)
+
+	sm := mux.NewRouter()
+
+	getR := sm.Methods(http.MethodGet).Subrouter()
+	getR.HandleFunc("/products", ph.ListAll)
+	getR.HandleFunc("/products/{id:[0-9]+}", ph.ListSingle)
+
+	putR := sm.Methods(http.MethodPut).Subrouter()
+	putR.HandleFunc("/products", ph.Update)
+	putR.Use(ph.MiddlewareValidateProduct)
+
+	postR := sm.Methods(http.MethodPost).Subrouter()
+	postR.HandleFunc("/products", ph.Create)
+	postR.Use(ph.MiddlewareValidateProduct)
+
+	deleteR := sm.Methods(http.MethodDelete).Subrouter()
+	deleteR.HandleFunc("/products/{id:[0-9]+}", ph.Delete)
+
+	// handler for documentation
+	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.Redoc(opts, nil)
+
+	// can now go to localhost:9090/docs to see docs (JS generated)
+	getR.Handle("/docs", sh)
+	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+
+	//manually creating a server
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm, 
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// the ListenAndServe will not block since it is now wrapped up in a go function
+	go func() {
+		// creates the web server - to handle requests, http handlers have to be implemented 
+		// first parameter is bind address and second is http handler
+		// registers a default handler and uses default serve mux if we specify nil for second parameter
+		// a handler in go http is just an interface which has the ServeHTTP(ResponseWriter, *Request) method
+		//http.ListenAndServe(":9090", sm)
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	// broadcast a notification to channel whenever kill command/interrupt is received
+	signal.Notify(sigChan, os.Kill)
+
+	// reading from a channel will block until message is consumed
+	// once the message is consumed, server is shutdown
+	sig := <- sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	// graceful shutdown waits for requests to be done until shutting down the server
+	// useful for finishing up database transactions
+	// first create a context with duration of 30 seconds (give graceful shutdown 30 sec until forcefully shutting down)
+	tc, _ := context.WithTimeout(context.Background(), 30 * time.Second)
+	s.Shutdown(tc)
+}
